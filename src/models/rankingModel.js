a66c9a8e635885bb9e655aeeeba49d215711f4ff
henrique-olivier <henrique.rosa@sptech.school>
2024-06-06 19:57:38 -0300

    corrigindo select ranking

--- src/models/rankingModel.js
+++ src/models/rankingModel.js
@@ -5,7 +5,7 @@ function MontarRanking() {
   var instrucaoSql = `select usuario.nome as Nome, registroJogo.TempoEmSegundos as Tempo, registroJogo.movimentos as Movimentos, DATE_FORMAT(ranking.dtJogo, '%d/%m/%Y') as data from ranking 
   join usuario on ranking.fkUsuario= usuario.idusuario 
   join registroJogo on ranking.fkRegistro = registroJogo.idjogo
-  order by tempo asc
+  order by tempo asc, movimentos asc 
   limit 10;`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
